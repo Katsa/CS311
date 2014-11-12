f473e25db8194d2bec1786eab4b947bd78f1420c
Enrique Dupleich <nudlz7@gmail.com>
2014-11-12 16:04:43 -0500

    new version

--- Project 5/src/textGenerator.java
+++ Project 5/src/textGenerator.java
@@ -1,115 +1,58 @@
-
-import java.util.*;
-import java.math.*;
 import java.io.*;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+class test  {
 
-public class textGenerator {
-	
-	
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		String file="";
-		
-		while(!file.equals("q")){
-			
-			//get info on what we're reading and how we'll process
-			System.out.print("Please enter a training file, q to quit: ");
-			file = scan.nextLine();
-		
-			
-			System.out.print("Please enter the k value: ");
-			int k = Integer.parseInt(scan.nextLine());
-			
-			
-			int numGen = 0;
-			while(numGen <=0 ){
-				System.out.print("Enter number of characters to generate: ");
-				numGen = Integer.parseInt(scan.nextLine());
-			}
-			
-			generateText(getText(file,k), numGen, k);
-			System.out.println();
-		}
-	}
-	
-	//Trainer: sets up the transitional probabilities for the given k. You may modify this method header.
-	public void train(String file, int k){
-	}
-
-	public static HashMap getText(String filename, int k) {
-		HashMap map = new HashMap();
-		BufferedReader read = null;
-		 try {
-		 	String thisLine;
-		 	String gramX;
-		 	read = new BufferedReader(new FileReader(filename));
-		 	thisLine = read.readLine();
-		 	while(thisLine != null) {
-
-				int count = 0;
-
-				for(int i = 0; i < thisLine.length()/k; i++) {
-					gramX = thisLine.substring(count, k + 1);
-					String otherGramS = gramX.substring(1) + thisLine.substring(k+1, k+2);
-					String nextGramS = thisLine.substring(count+1, k+2);
-
-					if(map.containsValue(gramX)){
-						map.put(gramX, nextGramS);
-					} else {
-						ArrayList al = new ArrayList();
-						al.add(nextGramS);
-						map.put(gramX,al);
-					}
-					count++;
+  public static void main (String args[]) {
+  
+    HashMap<String,ArrayList<String>> map = new HashMap<String,ArrayList<String>>();
+    try {
+		for(String thisLine:readFile("baba.txt")){
+			int count=0;
+			int k=3;
+			while((thisLine.length()-count)>=k){
+				String gramX = thisLine.substring(count, k+count);
+				String otherGramS;
+				if ((thisLine.length()-count)>k){
+					otherGramS = gramX.substring(1) + thisLine.substring(k+count, k+count+1);
+				}else{
+					otherGramS = gramX.substring(1);					
+				}if(map.containsKey(gramX)){
+					map.get(gramX).add(otherGramS);
+				} else {
+					ArrayList<String> al = new ArrayList<String>();
+					al.add(otherGramS);
+					map.put(gramX,al);
 				}
+				count++;
 			}
-		 } catch (IOException e) {
-		 	e.printStackTrace();
-		 } finally {
-		 	try {
-		 		if(read != null)read.close();
-		 	} catch (IOException er) {
-		 		er.printStackTrace();
-		 	}
-		 }
-		 //System.out.println(grams.get(gramX));
-		return map;
-
-	}
-	
-	//Generator: Generates numGen characters based on the transitional probabilities estimated by the trainer 
-	public static void generateText(HashMap<String,ArrayList<String>> map, int numGen, int k){
-		int size = map.size();
-		Random randomGen =  new Random();
-		
-		Set set = map.keySet();
-		String[] keys = new String[size];
-		set.toArray(keys);
-		
-		int randomInt = randomGen.nextInt(map.size());
-		String gramX = keys[randomInt];
-		
-		//ArrayList<String> X = map.get(randomInt);
-		//String gramX = X.get(0);
-
-		for(int i = 0; i < size; i++) {
-			System.out.println(gramX);
-			ArrayList temp = map.get(gramX);
-			int randomInt2 = randomGen.nextInt((map.get(randomInt)).size());
-
-			String gramS = (String) temp.get(randomInt2);
-			String gramC =gramS.substring(k-1);
-			System.out.println(gramC);
-
-			String gramN = gramX.substring(k-1) + gramC;
-			gramX = gramN;
 		}
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	
-	
-	
+
+} // end main
+
+private static ArrayList<String> readFile(String filename)
+throws Exception
+{
+    String line = null;
+    ArrayList<String> records = new ArrayList<String>();
+ 
+    // wrap a BufferedReader around FileReader
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+ 
+    // use the readLine method of the BufferedReader to read one line at a time.
+    // the readLine method returns null when there is nothing else to read.
+    while ((line = bufferedReader.readLine()) != null)
+    {
+        records.add(line);
+    }
+   
+    // close the BufferedReader when we're done
+    bufferedReader.close();
+    return records;
+}
 }
