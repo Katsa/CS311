f65f4b0a501a1cddb56097e73c763af89891a174
Katsa <gykatsaounis@gmail.com>
2014-11-11 21:32:57 -0500

    no errors, but doesnt run properly

--- Project 5/src/textGenerator.java
+++ Project 5/src/textGenerator.java
@@ -25,63 +25,23 @@ public class textGenerator {
 			int k = Integer.parseInt(scan.nextLine());
 			
 			
-			//int numGen = 0;
-			//while(numGen <=0 ){
-			//	System.out.print("Enter number of characters to generate: ");
-			//	numGen = Integer.parseInt(scan.nextLine());
-			//}
-			getText(file,k);
+			int numGen = 0;
+			while(numGen <=0 ){
+				System.out.print("Enter number of characters to generate: ");
+				numGen = Integer.parseInt(scan.nextLine());
+			}
+			
+			generateText(getText(file,k), numGen, k);
 			System.out.println();
 		}
 	}
 	
 	//Trainer: sets up the transitional probabilities for the given k. You may modify this method header.
 	public void train(String file, int k){
-		HashMap<Integer,ArrayList<String>> map = new HashMap();
-		BufferedReader read = null;
-		try {
-			String thisLine;
-			String gramX;
-			String gramS;
-			read =  new BufferedReader(new FileReader(file));
-			thisLine = read.readLine();
-			while(thisLine != null) {
-				int count = 0;
-				int location = 0;
-
-				for(int i = 0; i < thisLine.length()/k; i++) {
-					gramX = thisLine.substring(count, k + 1);
-					//gramS = gramX.substring(1) + thisLine.substring(k+1, k+2);
-					gramS = thisLine.substring(count+1, k+2);
-
-					if(map.containsValue(gramX)) {
-						map.put(,gramS)
-					}
-					else {
-						ArrayList<String> al = new ArrayList();
-						al.add(gramX);
-						al.add(gramS);
-						map.put(,al)
-						location ++;
-					}
-					count ++;
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(read != null)read.close();
-		 	} catch (IOException er) {
-	 			er.printStackTrace();
-		 	}
-		}
-
-		
 	}
 
-	public void getText(String filename, int k) {
-		HashMap grams = new HashMap();
+	public static HashMap getText(String filename, int k) {
+		HashMap map = new HashMap();
 		BufferedReader read = null;
 		 try {
 		 	String thisLine;
@@ -97,12 +57,12 @@ public class textGenerator {
 					String otherGramS = gramX.substring(1) + thisLine.substring(k+1, k+2);
 					String nextGramS = thisLine.substring(count+1, k+2);
 
-					if(grams.containsValue(gramX)){
-						grams.put(gramX, nextGramS);
+					if(map.containsValue(gramX)){
+						map.put(gramX, nextGramS);
 					} else {
 						ArrayList al = new ArrayList();
 						al.add(nextGramS);
-						grams.put(gramX,al);
+						map.put(gramX,al);
 					}
 					count++;
 				}
@@ -116,21 +76,29 @@ public class textGenerator {
 		 		er.printStackTrace();
 		 	}
 		 }
-
+		 //System.out.println(grams.get(gramX));
+		return map;
 
 	}
 	
 	//Generator: Generates numGen characters based on the transitional probabilities estimated by the trainer 
-	public static void generateText(HashMap<Integer,ArrayList<String>> map, int size, int numGen, int k){
+	public static void generateText(HashMap<String,ArrayList<String>> map, int numGen, int k){
+		int size = map.size();
 		Random randomGen =  new Random();
+		
+		Set set = map.keySet();
+		String[] keys = new String[size];
+		set.toArray(keys);
+		
 		int randomInt = randomGen.nextInt(map.size());
-
-		ArrayList<String> X = map.get(randomInt);
-		String gramX = X.get(0);
+		String gramX = keys[randomInt];
+		
+		//ArrayList<String> X = map.get(randomInt);
+		//String gramX = X.get(0);
 
 		for(int i = 0; i < size; i++) {
 			System.out.println(gramX);
-			ArrayList temp = map.get(randomInt);
+			ArrayList temp = map.get(gramX);
 			int randomInt2 = randomGen.nextInt((map.get(randomInt)).size());
 
 			String gramS = (String) temp.get(randomInt2);
