32f066f93f92810adbd2e50ef6a3c2dde5247378
edupleich <nudlz7@gmail.com>
2014-11-27 17:13:53 -0500

    Update spamFilter.java
    preprocess is working, though not as efficient as one would hope.

--- Project 6/src/spamFilter.java
+++ Project 6/src/spamFilter.java
@@ -1,13 +1,12 @@
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class spamFilter {
 
-	final static File allfolder = new File("Users/Katsa1/Documents/College/Junior/Project 6/spamdata/");
-	final static File spamfolder = new File("Users/Katsa1/Documents/College/Junior/Project 6/spamdata/spam/");
-	final static File hamfolder = new File("Users/Katsa1/Documents/College/Junior/Project 6/spamdata/ham/");
+	final static File allfolder = new File("/Users/nudlz/Desktop/CS/CS311/HW6/src/spamdata/");
+	final static File spamfolder = new File("/Users/nudlz/Desktop/CS/CS311/HW6/src/spamdata/spam/");
+	final static File hamfolder = new File("/Users/nudlz/Desktop/CS/CS311/HW6/src/spamdata/ham/");
 	
 	public static void main(String[] args) throws IOException
 	{		
@@ -22,8 +21,8 @@ public class spamFilter {
 		
 		
 		
-		
-    System.out.println("all messages:");
+		/*
+        System.out.println("all messages:");
 		readFiles(allfolder);		
 		System.out.println();
 		
@@ -33,7 +32,8 @@ public class spamFilter {
 		
 		System.out.println("ham messages:");
 		readFiles(hamfolder);
-		
+		*/
+		System.out.println(Arrays.toString(preprocess(spamfolder,50)));
 	}
 	
 
@@ -41,32 +41,15 @@ public class spamFilter {
 	 * Takes as parameter the name of a folder and returns a list of filenames (Strings) 
 	 * in the folder.
 	 */
-	public static ArrayList<String> readFiles(File folder) throws IOException{
-			String line = null;
-			//List to store filenames in folder
-			ArrayList<String> filelist = new ArrayList<String>();
-			// wrap a BufferedReader around FileReader
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(folder));
-	 		while ((line = bufferedReader.readLine()) != null) {
-		    	String[] temp = line.split(" ");
-		    	for(String x: temp)
-		    		filelist.add(x);
-	 		}
-	    
-		    String[] wordString = new String[filelist.size()];
-		    for(int i =0; i < filelist.size(); i++) {
-	    		wordString[i] = filelist.get(i);
-		    }
-	   
-		    // close the BufferedReader
-		    bufferedReader.close();
+	public static ArrayList<String> readFiles(File folder){
+		
+		//List to store filenames in folder
+		ArrayList<String> filelist = new ArrayList<String>();
+		
+		//call to recursive method that reads all filenames in folder
+		listFilesForFolder(folder, filelist );
 
-			//call to recursive method that reads all filenames in folder
-			listFilesForFolder(folder, filelist );
-
-			return filelist;
-
-			
+		return filelist;
 	}
 	
 	
@@ -82,21 +65,77 @@ public class spamFilter {
 	        else {
 	        	String filename = fileEntry.getName();
 	        	files.add(filename);
-	            System.out.println(filename);
+	            //System.out.println(filename);
 	        }
 	    }
 	}
-
+	//Helper function that returns K most frequent words in a hashmap.
+	public static ArrayList<String> kmostfreq(HashMap<String, Integer> map, int k) { 
+		List<Map.Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(map.entrySet());
+		//Sorting by value
+		Collections.sort(list, new Comparator() {
+			public int compare(Object o1, Object o2) {
+				return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+	            	}});
+	      	
+		ArrayList<String> result = new ArrayList<String>();
+		for (int i = list.size()-1;i>=list.size()-k;i--) {
+			Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) list.get(i);
+			result.add(entry.getKey());
+		} 
+		return result;
+	}
+	
+	
 	
 	/*
 	 * TO DO
 	 * Preprocessor: Reads email messages to fill a table of features.
 	 * You may modify the method header (return type, parameters) as you see fit.
 	 */
-	public static void preprocess()
+	public static String[] preprocess(File folder, int k) throws IOException
 	{
+		//the fifty most common words in English according to wikpedia
+		String[] commonWords = {"the","be","to","of","and","a","in","that",
+				"have","I","it","for","not","on","with","he","as","you","do",
+				"at","this","but","his","by","from","they","we","say","her",
+				"she","or","an","are","will","my","one","all","would","there","their",
+				"what","so","up","out","if","about","who","get","which","go","me",
+				"when","make","can","like","time","no","just","him","know","take",
+				"people","into","year","your","good","some","could","them","see",
+				"other","than","then","now","look","only","come","its","over","think",
+				"also","back","after","use","subject","two","how","i","our","work","first","well",
+				"way","even","new","want","because","any","these","give","day","most","us","is"};
+		//Common words into arrayList so we can use contains method
+		ArrayList<String> commons =new ArrayList<String>(Arrays.asList(commonWords));
+		HashMap<String, Integer> freqz = new HashMap<String, Integer>();
+		ArrayList<String> filez = readFiles(folder);
+		//Randomly select files from spamdata to get our features.
+		Random randomGen = new Random();
+		for (int i =0; i<100; i++){
+			int randInt = randomGen.nextInt(filez.size());
+		    BufferedReader bufferedReader = new BufferedReader(new FileReader("src/spamdata/spam/"+filez.get(randInt)));
+		    String line = null;
+		    while ((line = bufferedReader.readLine()) != null)
+		    {
+		    	String[] temp = line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+		    	for(String xt: temp){
+		    		if(!commons.contains(xt)){
+		    			if(freqz.containsKey(xt)){
+		    				freqz.put(xt, freqz.get(xt)+1);
+		    			}else{
+		    				freqz.put(xt, 1);
+		    			}
+		    		}		
+		    	}
+		    }
+		    bufferedReader.close();
+		}
+		return kmostfreq(freqz,k).toArray(new String[k]);
 		
+       
 	}
+	
 	
 	/*
 	 * TO DO
